943f8bc04552d0a394662fad20698803176f8fba
Hickups789 <jackysantiago358@yahoo.com>
2022-09-02 20:57:42 -0400

    routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -14,15 +14,53 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    username: req.body.username,
+  
+  })
+    .then((dbCatagoryData) => res.json(dbCatagoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCatagoryData) => {
+      if (!dbCatagoryData[0]) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbCatagoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((dbCatagoryData) => {
+    if (!dbCatagoryData) {
+      res.status(404).json({ message: "No user found with this id" });
+      return;
+    }
+    res.json(dbCatagoryData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
